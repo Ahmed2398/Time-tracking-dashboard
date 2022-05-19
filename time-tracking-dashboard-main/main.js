4ba78d16fb2ac50249265df9eb4d2d8e2ebfbe0a
ahmed2398 <ahmedashraf2398>
2022-05-19 09:57:31 +0200

    updat data.json Url

--- time-tracking-dashboard-main/main.js
+++ time-tracking-dashboard-main/main.js
@@ -6,44 +6,44 @@ const mothlyBtn = document.querySelector('#monthly-btn');
 const hours = document.querySelectorAll('.hours');
 const lastTime = document.querySelectorAll('.last-time');
 
-function dailyData(){
-    fetch('/time-tracking-dashboard-main/data.json')
+function dailyData() {
+    fetch('https://ahmed2398.github.io/Time-tracking-dashboard/time-tracking-dashboard-main/data.json')
         .then(response => response.json())
         .then(data => {
-            hours.forEach((e,i) => {
+            hours.forEach((e, i) => {
                 hours[i].innerHTML = data[i].timeframes.daily.current + 'hrs';
                 lastTime[i].innerHTML = 'Last Daily - ' + data[i].timeframes.daily.previous + 'hrs';
             })
         })
 }
 
-function weeklyData(){
-    fetch('/time-tracking-dashboard-main/data.json')
-    .then(response => response.json())
-    .then(data => {
-        hours.forEach((e,i) => {
-            hours[i].innerHTML = data[i].timeframes.weekly.current + 'hrs';
-            lastTime[i].innerHTML = 'Last Day - ' + data[i].timeframes.weekly.previous + 'hrs';
+function weeklyData() {
+    fetch('https://ahmed2398.github.io/Time-tracking-dashboard/time-tracking-dashboard-main/data.json')
+        .then(response => response.json())
+        .then(data => {
+            hours.forEach((e, i) => {
+                hours[i].innerHTML = data[i].timeframes.weekly.current + 'hrs';
+                lastTime[i].innerHTML = 'Last Day - ' + data[i].timeframes.weekly.previous + 'hrs';
+            })
         })
-    })
 }
 
 
-function monthlyData(){
-    fetch('/time-tracking-dashboard-main/data.json')
-    .then(response => response.json())
-    .then(data => {
-        hours.forEach((e,i) => {
-            hours[i].innerHTML = data[i].timeframes.monthly.current + 'hrs';
-            lastTime[i].innerHTML = 'Last Day - ' + data[i].timeframes.monthly.previous + 'hrs';
+function monthlyData() {
+    fetch('https://ahmed2398.github.io/Time-tracking-dashboard/time-tracking-dashboard-main/data.json')
+        .then(response => response.json())
+        .then(data => {
+            hours.forEach((e, i) => {
+                hours[i].innerHTML = data[i].timeframes.monthly.current + 'hrs';
+                lastTime[i].innerHTML = 'Last Day - ' + data[i].timeframes.monthly.previous + 'hrs';
+            })
         })
-    })
 }
 
 
 
 
-dailyBtn.addEventListener('click' , () => {
+dailyBtn.addEventListener('click', () => {
     dailyBtn.classList.remove('text-muted')
     weeklyBtn.classList.add('text-muted')
     mothlyBtn.classList.add('text-muted')
@@ -56,7 +56,7 @@ weeklyBtn.addEventListener('click', () => {
     dailyBtn.classList.add('text-muted')
     mothlyBtn.classList.add('text-muted')
     weeklyData()
-} )
+})
 
 
 mothlyBtn.addEventListener('click', () => {
